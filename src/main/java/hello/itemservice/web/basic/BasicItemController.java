96c4f75b32c3eedd6f84b9a6e54577da218a578c
Kimhyunjin4455 <dp4455@naver.com>
2023-08-24 21:47:28 +0900

    0824Pdf6-020

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,42 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final 붙은 변수 베이스로 생성자 만듬
+public class BasicItemController {
+    /**
+     * 생성자 주입, 스프링에서 생성자가 1개일 때는 @Autowired 생략 가능
+    private final ItemRepository itemRepository;
+    @Autowired
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    */
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){ // 아이템 목록 출력
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostMapping
+    public void init(){ // 테스트 목적
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
